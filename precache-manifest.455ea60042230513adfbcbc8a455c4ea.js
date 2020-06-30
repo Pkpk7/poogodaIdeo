b20a992ad7e0c024b7ee03fafd337b106884f569
test <pawelkluk12@interia.pl>
2020-06-30 13:17:58 +0200

    Updates

---
+++ precache-manifest.455ea60042230513adfbcbc8a455c4ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b65c44b057004f0c9249807ce9ea60b",
+    "url": "/poogodaIdeo/index.html"
+  },
+  {
+    "revision": "d5391167c58e77e865ee",
+    "url": "/poogodaIdeo/static/css/2.c310cfc4.chunk.css"
+  },
+  {
+    "revision": "b4a12f4748b6c1b6bcb0",
+    "url": "/poogodaIdeo/static/css/main.78714f87.chunk.css"
+  },
+  {
+    "revision": "d5391167c58e77e865ee",
+    "url": "/poogodaIdeo/static/js/2.c5087410.chunk.js"
+  },
+  {
+    "revision": "52e1883c0a2d5e9c4ef66f4aec17d9d8",
+    "url": "/poogodaIdeo/static/js/2.c5087410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a12f4748b6c1b6bcb0",
+    "url": "/poogodaIdeo/static/js/main.dbc0ca83.chunk.js"
+  },
+  {
+    "revision": "0137b9135300e80d6b53",
+    "url": "/poogodaIdeo/static/js/runtime-main.d97906ff.js"
+  },
+  {
+    "revision": "9692b8de936f613fb572897368eee73f",
+    "url": "/poogodaIdeo/static/media/Logo.9692b8de.svg"
+  }
+]);
